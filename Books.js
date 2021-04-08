aee18dd835305c1aee107068cbaeae4607efe905
puppets101 <oscand92@gmail.com>
2021-04-08 12:00:22 +0200

    Added .rest + func for GET all/single book and POST (add) book

---
+++ Books.js
@@ -0,0 +1,22 @@
+const books = [
+  {
+    id: 1,
+    name: "Eloquent JavaScript",
+    author: "Marjin Haverbeke",
+    genre: "Technology",
+  },
+  {
+    id: 2,
+    name: "Sapiens",
+    author: "Yuval Noah Harrari",
+    genre: "History",
+  },
+  {
+    id: 3,
+    name: "Flow",
+    author: "Mihaly Csikszentmihalyi",
+    genre: "Psychology",
+  },
+];
+
+module.exports = books;
--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require("express");
+const books = require("./Books");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("REST API - BOOKS");
+});
+
+// Get books
+app.get("/api/books", (req, res) => {
+  res.send(books);
+});
+
+// Get single book
+app.get("/api/books/:id", (req, res) => {
+  const book = books.find((book) => book.id === parseInt(req.params.id));
+  if (!book) {
+    res
+      .status(404)
+      .send({ msg: `There's no book with the id of ${req.params.id}` });
+    return;
+  }
+  res.send(book);
+});
+
+app.post("/api/books", (req, res) => {
+  const newBook = {
+    id: books.length + 1,
+    name: "Superintelligence",
+    author: "Nick Boström",
+    genre: "Science",
+  };
+  books.push(newBook);
+  res.send(books);
 });
 
 app.listen(port, () => console.log(`Running on port http://localhost:${port}`));
