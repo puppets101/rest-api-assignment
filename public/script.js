b1e57dbb46fc8f5ebb3922a1cd31e6309cd50d1f
puppets101 <oscand92@gmail.com>
2021-04-15 11:21:23 +0200

    Added func to delete specific book + clean up code

--- public/script.js
+++ public/script.js
@@ -6,18 +6,26 @@ async function main() {
 }
 
 async function addEventListeners() {
-  // Handle form submits
-  const form = document.getElementById("form");
-  form.addEventListener("submit", handleAddBookFormSubmit);
+  // Handle add book form submits
+  const addBookForm = document.getElementById("addBookForm");
+  addBookForm.addEventListener("submit", handleAddBookFormSubmit);
 
   const handleSpecificIdInput = document.getElementById("getSpecificBook");
   handleSpecificIdInput.addEventListener("submit", handleGetSpecificBookSubmit);
 
-  // TODO - UPDATE AND DELETE
-
   // Get all books
   const allBooksBtn = document.getElementById("allBooks");
   allBooksBtn.addEventListener("click", getAllBooks);
+}
+
+// Helper functions
+function clearUl() {
+  const ul = document.getElementById("list");
+  ul.innerHTML = "";
+}
+function clearInputFields() {
+  const addBookForm = document.getElementById("addBookForm");
+  addBookForm.reset();
 }
 
 function handleAddBookFormSubmit(e) {
@@ -38,6 +46,7 @@ function handleAddBookFormSubmit(e) {
   };
   const { title, author, genre } = newBook;
   addBook(title, author, genre);
+  clearInputFields();
 }
 
 function handleGetSpecificBookSubmit(e) {
@@ -48,69 +57,45 @@ function handleGetSpecificBookSubmit(e) {
   getSpecificBook(idValue);
 }
 
-function handleUpdateBookFormSubmit() {
-  console.log("Book Updated");
-
-  const idInput = document.getElementById("idInput");
-  const idValue = idInput.value;
-  const titleInput = document.getElementById("titleInput");
-  const titleValue = titleInput.value;
-  const authorInput = document.getElementById("authorInput");
-  const authorValue = authorInput.value;
-  const genreInput = document.getElementById("genreInput");
-  const genreValue = genreInput.value;
-
-  const updatedBook = {
-    id: idValue,
-    title: titleValue,
-    author: authorValue,
-    genre: genreValue,
-  };
-  const { id, title, author, genre } = updatedBook;
-  updateBook(id, title, author, genre);
-}
-
-function clearParagraph() {
-  const p = document.getElementById("para");
-  p.innerHTML = "";
-}
-function clearUl() {
-  const ul = document.getElementById("list");
-  ul.innerHTML = "";
+function handleUpdateBook() {
+  const book = this;
+  console.log(book);
 }
 
 async function addBook(title, author, genre) {
   const body = { title: title, author: author, genre: genre };
   const book = await requestResource("/api/books", "POST", body);
+  getAllBooks();
   return book;
 }
 
 async function getAllBooks() {
   const books = await requestResource("/api/books", "GET");
-  clearParagraph();
   const ul = document.getElementById("list");
   ul.innerHTML = "";
   books.forEach((book) => {
     const li = document.createElement("li");
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("btn");
-    deleteBtn.innerHTML = "Delete";
-    deleteBtn.addEventListener("click", deleteBook);
+    deleteBtn.innerHTML = '<i class="fa fa-remove"></i>';
+    deleteBtn.addEventListener("click", deleteBook.bind(book.id));
     const updateBtn = document.createElement("button");
-    updateBtn.classList.add("btn");
-    updateBtn.innerHTML = "Edit";
-    updateBtn.addEventListener("click", updateBook);
+    updateBtn.classList.add("btn", "p-0", "mx-2");
+    updateBtn.innerHTML = '<i class="fa fa-edit"></i>';
+    updateBtn.addEventListener("click", handleUpdateBook.bind(book));
+
     li.appendChild(document.createTextNode(book.title + " - "));
     li.appendChild(document.createTextNode(book.author + " - "));
     li.appendChild(document.createTextNode(book.genre));
-    ul.appendChild(li).appendChild(deleteBtn).appendChild(updateBtn);
+    li.appendChild(updateBtn);
+    li.appendChild(deleteBtn);
+    ul.appendChild(li);
   });
   return books;
 }
 
 async function getSpecificBook(id) {
   const book = await requestResource("/api/books/" + id, "GET");
-  clearParagraph();
   const ul = document.getElementById("specificBook");
   ul.innerHTML = "";
   const li = document.createElement("li");
@@ -124,19 +109,14 @@ async function getSpecificBook(id) {
 async function updateBook(id, title, author, genre) {
   const body = { id: id, title: title, author: author, genre: genre };
   const book = await requestResource("/api/books/" + id, "PUT", body);
-  const p = document.getElementById("para");
-  clearUl();
-  p.innerHTML = "Book updated";
   return book;
 }
 
-async function deleteBook(id) {
-  console.log("deleted" + id);
-  const book = await requestResource("/api/books/" + id, "DELETE");
-  // const p = document.getElementById("para");
-  // clearUl();
-  // p.innerHTML = "Book deleted";
-  return book;
+async function deleteBook() {
+  console.log("deleted " + this);
+  const id = this;
+  await requestResource("/api/books/" + id, "DELETE");
+  getAllBooks();
 }
 
 async function requestResource(url, method, body) {
